0d09e52077eadeee356a0a68b6e91c5b786707e5
pompy <nicole.lynn.pompei@gmail.com>
2020-11-13 18:33:40 -0500

    aded arraydeque and hashmap tests

--- gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
@@ -1,4 +1,64 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestArrayDeque {
+
+    @Test
+    public void arrayDequeAddTest(){
+    Person jon = new  Person("Jon",1994);
+    Person jessie = new Person("Jessie",1990);
+    Person kyle = new Person("Kyle",1992);
+
+    Person[] jonNJessie = {jon, jessie};
+    List<Person> list = Arrays.asList(jonNJessie);
+    ArrayDeque<Person> actual = new ArrayDeque<>(list);
+
+    actual.add(kyle);
+
+        assertEquals(true, actual.contains(kyle));
+    }
+
+    @Test
+    public void arrayDequeAddFirstTest(){
+        Person jon = new  Person("Jon",1994);
+        Person jessie = new Person("Jessie",1990);
+        Person kyle = new Person("Kyle",1992);
+
+        Person[] jonNJessie = {jon, jessie};
+        List<Person> list = Arrays.asList(jonNJessie);
+        ArrayDeque<Person> actual = new ArrayDeque<>(list);
+        actual.addFirst(kyle);
+        Person actualPerson = actual.getFirst();
+        Person expectedPerson = kyle;
+
+        Assert.assertEquals(expectedPerson, actualPerson);
+
+    }
+
+    @Test
+    public void arrayDequeAddLastTest(){
+        Person jon = new  Person("Jon",1994);
+        Person jessie = new Person("Jessie",1990);
+        Person kyle = new Person("Kyle",1992);
+
+        Person[] jonNJessie = {jon, jessie};
+        List<Person> list = Arrays.asList(jonNJessie);
+        ArrayDeque<Person> actual = new ArrayDeque<>(list);
+        actual.addLast(kyle);
+
+        Person actualPerson = actual.getLast();
+        Person expectedPerson = kyle;
+
+        Assert.assertEquals(expectedPerson, actualPerson);
+
+
+    }
 }
--- gettestie/src/test/java/rocks/zipcode/TestHashMap.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashMap.java
@@ -1,4 +1,53 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestHashMap {
+
+    @Test
+    public void hashMapSizeTest(){
+        Person jessie = new Person ("Jessie", 1990);
+        Person kyle = new Person ("Kyle", 1983);
+        Person jon = new Person ("Jon", 1989);
+
+       HashMap <String, Integer> people = new HashMap();
+
+       people.put("Jessie", 1990);
+       people.put("Kyle", 1983);
+       people.put("Jon", 1989);
+
+       int expected = 3;
+       int actual = people.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void hashMapPutTest(){
+        HashMap <String, Integer> people = new HashMap();
+
+        people.put("Jessie", 1990);
+        people.put("Kyle", 1983);
+        people.put("Jon", 1989);
+
+        assertEquals(false, people.isEmpty());
+
+    }
+
+    @Test
+    public void hashMapRemoveTest(){
+        HashMap <String, Integer> people = new HashMap();
+
+        people.put("Jessie", 1990);
+        people.remove("Jessie");
+
+        assertEquals(true, people.isEmpty());
+
+
+    }
 }
