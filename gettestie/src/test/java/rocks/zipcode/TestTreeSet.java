291f3988166f056ca31a47e31e4a6a78d8ba2e63
pompy <nicole.lynn.pompei@gmail.com>
2020-11-13 19:29:43 -0500

    added tree set tests

--- gettestie/src/test/java/rocks/zipcode/TestTreeSet.java
+++ gettestie/src/test/java/rocks/zipcode/TestTreeSet.java
@@ -1,4 +1,53 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.TreeSet;
+
 public class TestTreeSet {
+
+    @Test
+    public void treeSetFirstTest(){
+        TreeSet<String> people = new TreeSet();
+
+        people.add("Jessie");
+        people.add("Kyle");
+        people.add("Jon");
+
+        String expected = "Jessie";
+        String actual = people.first();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void treeSetLastTest(){
+        TreeSet<String> people = new TreeSet();
+
+        people.add("Jessie");
+        people.add("Kyle");
+        people.add("Jon");
+
+        String expected = "Kyle";
+        String actual = people.last();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void treeSetPollFirstTest(){
+        TreeSet<String> people = new TreeSet();
+
+        people.add("Jessie");
+        people.add("Kyle");
+        people.add("Jon");
+
+        String expected = "Jessie";
+        String actual = people.pollFirst();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
