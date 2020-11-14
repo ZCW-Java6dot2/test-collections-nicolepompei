49149989114814dcb2be12e79f3d516aec401cd7
pompy <nicole.lynn.pompei@gmail.com>
2020-11-13 20:45:10 -0500

    added vector tests

--- gettestie/src/test/java/rocks/zipcode/TestVector.java
+++ gettestie/src/test/java/rocks/zipcode/TestVector.java
@@ -1,4 +1,52 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.util.Vector;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestVector {
+    @Test
+    public void testVectorLastElement(){
+        Vector<String> people = new Vector<>();
+
+        people.add("Jessie");
+        people.add("Kyle");
+        people.add("Jon");
+
+        String expected = "Jon";
+        String actual = people.lastElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testVectorFirstElement(){
+        Vector<String> people = new Vector<>();
+
+        people.add("Jessie");
+        people.add("Kyle");
+        people.add("Jon");
+
+        String expected = "Jessie";
+        String actual = people.firstElement();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testVectorClear(){
+        Vector<String> people = new Vector<>();
+
+        people.add("Jessie");
+        people.add("Kyle");
+        people.add("Jon");
+
+        people.clear();
+
+        assertEquals(true,people.isEmpty());
+    }
 }
